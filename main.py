3d26d14540062a11061abd16cd10782e23a5707d
hkbridges <hastenkbridges@gmail.com>
2026-05-16 16:49:19 -0500

    Read book text and print to console

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath: str) -> str:
+    with open(filepath) as file:
+        book_text = file.read()
+    return book_text
+
+frankenstein_filepath = "books/frankenstein.txt"
+
+def main():
+    print(get_book_text(frankenstein_filepath))
+
+main()
